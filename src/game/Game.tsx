6166e28b64ef41775c257dcc01ceea42207022bb
Léa Castanet <lea.castanet@hotmail.com>
2023-01-25 17:07:22 +0100

    creation useInterval

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,17 +1,23 @@
 import React, { useRef } from "react";
 import Canvas from "../canvas/Canvas";
 import { GameWrapper } from "./Game.styles";
+import useGameLogic from "./useGameLogic";
+import draw from "../draw/draw";
 
 interface GameProps {}
 
 const Game: React.FC<GameProps> = ({}) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const draw = (ctx: CanvasRenderingContext2D) => {};
+  const { snakeBody } = useGameLogic();
+
+  const drawGame = (ctx: CanvasRenderingContext2D) => {
+    draw({ ctx, snakeBody });
+  };
 
   return (
     <GameWrapper>
-      <Canvas draw={draw} ref={canvasRef} />
+      <Canvas draw={drawGame} ref={canvasRef} />
     </GameWrapper>
   );
 };
