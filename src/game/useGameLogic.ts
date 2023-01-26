405c97870ccfc3415c8bdda667bdfc6d800c18cf
Léa Castanet <lea.castanet@hotmail.com>
2023-01-26 12:12:42 +0100

    border non franchissables

--- src/game/useGameLogic.ts
+++ src/game/useGameLogic.ts
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Canvas from "../canvas/Canvas";
+import { SEGMENT_SIZE } from "../draw/draw";
 import useInterval from "../utils/useInterval";
 import createSnakeMovement from "./movement";
 
@@ -16,7 +18,12 @@ export enum Direction {
 
 const MOVEMENT_SPEED = 100;
 
-const useGameLogic = () => {
+interface UseGameLogicArgs {
+  canvasWidth?: number;
+  canvasHeight?: number;
+}
+
+const useGameLogic = ({ canvasHeight, canvasWidth }: UseGameLogicArgs) => {
   const [direction, setDirection] = useState<Direction | undefined>();
   const [snakeBody, setSnakeBody] = useState<Position[]>([
     {
@@ -24,6 +31,8 @@ const useGameLogic = () => {
       y: 0,
     },
   ]);
+
+  const snakeHeadPosition = snakeBody[snakeBody.length - 1];
 
   const { moveDown, moveUp, moveLeft, moveRight } = createSnakeMovement();
 
@@ -49,98 +58,71 @@ const useGameLogic = () => {
           setDirection(Direction.LEFT);
         }
         break;
+      case "ArrowDown":
+        if (direction !== Direction.UP) {
+          setDirection(Direction.DOWN);
+        }
+        break;
+      case "ArrowUp":
+        if (direction !== Direction.DOWN) {
+          setDirection(Direction.UP);
+        }
+        break;
+      case "ArrowRight":
+        if (direction !== Direction.LEFT) {
+          setDirection(Direction.RIGHT);
+        }
+        break;
+      case "ArrowLeft":
+        if (direction !== Direction.RIGHT) {
+          setDirection(Direction.LEFT);
+        }
+        break;
     }
   };
-
-  // const moveSnake = () => {
-  //   let snakeBodyAfterMovement: Position[] | undefined;
-  //   switch (direction) {
-  //     case Direction.UP:
-  //       if (snakeHeadPosition.y > 0) {
-  //         snakeBodyAfterMovement = moveUp(snakeBody);
-  //       } else if (canvasWidth && snakeHeadPosition.x > canvasWidth / 2) {
-  //         setDirection(Direction.LEFT);
-  //       } else {
-  //         setDirection(Direction.RIGHT);
-  //       }
-  //       break;
-  //     case Direction.DOWN:
-  //       if (canvasHeight && snakeHeadPosition.y < canvasHeight - SEGMENT_SIZE) {
-  //         snakeBodyAfterMovement = moveDown(snakeBody);
-  //       } else if (canvasWidth && snakeHeadPosition.x > canvasWidth / 2) {
-  //         setDirection(Direction.LEFT);
-  //       } else {
-  //         setDirection(Direction.RIGHT);
-  //       }
-
-  //       break;
-  //     case Direction.LEFT:
-  //       if (snakeHeadPosition.x > 0) {
-  //         snakeBodyAfterMovement = moveLeft(snakeBody);
-  //       } else if (canvasHeight && snakeHeadPosition.y < canvasHeight / 2) {
-  //         setDirection(Direction.DOWN);
-  //       } else {
-  //         setDirection(Direction.UP);
-  //       }
-  //       break;
-  //     case Direction.RIGHT:
-  //       if (canvasWidth && snakeHeadPosition.x < canvasWidth - SEGMENT_SIZE) {
-  //         snakeBodyAfterMovement = moveRight(snakeBody);
-  //       } else if (canvasHeight && snakeHeadPosition.y < canvasHeight / 2) {
-  //         setDirection(Direction.DOWN);
-  //       } else {
-  //         setDirection(Direction.UP);
-  //       }
-  //       break;
-  //   }
-
-  //   //snake eats itself
-  //   if (snakeBodyAfterMovement) {
-  //     const isGameOver = hasSnakeEatenItself(snakeBodyAfterMovement);
-  //     if (isGameOver) {
-  //       onGameOver();
-  //     }
-  //   }
-
-  //   if (
-  //     direction !== undefined &&
-  //     foodPosition &&
-  //     willSnakeHitTheFood({
-  //       foodPosition,
-  //       snakeHeadPosition,
-  //       direction,
-  //     })
-  //   ) {
-  //     setSnakeBody([
-  //       ...snakeBodyAfterMovement!,
-  //       { x: foodPosition.x, y: foodPosition.y },
-  //     ]);
-
-  //     setFoodPosition({
-  //       x: randomPositionOnGrid({
-  //         threshold: canvasWidth!,
-  //       }),
-  //       y: randomPositionOnGrid({ threshold: canvasHeight! }),
-  //     });
-  //   } else if (snakeBodyAfterMovement) {
-  //     setSnakeBody(snakeBodyAfterMovement);
-  //   }
-  // };
 
   const moveSnake = () => {
     let snakeBodyAfterMovement: Position[] | undefined;
     switch (direction) {
       case Direction.UP:
-        snakeBodyAfterMovement = moveUp(snakeBody);
+        if (snakeHeadPosition.y > 0) {
+          snakeBodyAfterMovement = moveUp(snakeBody);
+        } else if (canvasWidth && snakeHeadPosition.x > canvasWidth / 2) {
+          setDirection(Direction.LEFT);
+        } else {
+          setDirection(Direction.RIGHT);
+        }
         break;
+
       case Direction.DOWN:
-        snakeBodyAfterMovement = moveDown(snakeBody);
+        if (canvasHeight && snakeHeadPosition.y < canvasHeight - SEGMENT_SIZE) {
+          snakeBodyAfterMovement = moveDown(snakeBody);
+        } else if (canvasWidth && snakeHeadPosition.x > canvasWidth / 2) {
+          setDirection(Direction.LEFT);
+        } else {
+          setDirection(Direction.RIGHT);
+        }
         break;
+
       case Direction.LEFT:
-        snakeBodyAfterMovement = moveLeft(snakeBody);
+        if (snakeHeadPosition.x > 0) {
+          snakeBodyAfterMovement = moveLeft(snakeBody);
+        } else if (canvasHeight && snakeHeadPosition.y < canvasHeight / 2) {
+          setDirection(Direction.DOWN);
+        } else {
+          setDirection(Direction.UP);
+        }
         break;
+
       case Direction.RIGHT:
-        snakeBodyAfterMovement = moveRight(snakeBody);
+        if (canvasWidth && snakeHeadPosition.x < canvasWidth - SEGMENT_SIZE) {
+          snakeBodyAfterMovement = moveRight(snakeBody);
+        } else if (canvasHeight && snakeHeadPosition.y < canvasHeight / 2) {
+          setDirection(Direction.DOWN);
+        } else {
+          setDirection(Direction.UP);
+        }
+
         break;
     }
     if (snakeBodyAfterMovement) {
